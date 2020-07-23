59832ff4142896b5e6c085dbef48ff77bf3faef9
Srishti Nath <srishti.nath@gmail.com>
2020-07-22 21:48:03 -0400

    added user room controller and resources

---
+++ app/controllers/user_rooms_controller.rb
@@ -0,0 +1,18 @@
+class UserRoomsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def index
+        @userRooms = UserRoom.all
+        render json: @userRooms
+    end
+    def create
+        @userRoom = UserRoom.create(userRoomParams)
+        render json: @userRoom.room
+    end
+
+    private
+
+    def userRoomParams
+        params.permit(:user_id, :room_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   delete '/clue_lists/:id/deleteItem/:item_id', to: "clue_lists#deleteItem"
   post '/users/login', to: 'users#login'
   get '/users/stay_logged_in', to: 'users#stay_logged_in'
-  resources :characters, :chats, :clue_lists, :items, :item_clue_lists, :locations, :rooms, :stories, :users
+  resources :characters, :chats, :clue_lists, :items, :item_clue_lists, :locations, :rooms, :stories, :users, :user_rooms
   # get '/item_clue_lists/:cluelist_id/:item_id', to: "item_clue_lists#show"
 
 end
