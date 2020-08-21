fd467e4fb3594f9bb065cbadd4d5de806a881889
Srishti Nath <srishti.nath@gmail.com>
2020-08-21 15:04:12 -0400

    enabled cors

--- config/application.rb
+++ config/application.rb
@@ -18,15 +18,15 @@ module HatshepsutBackend
 
     config.api_only = true
 
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource(
-    #       '*',
-    #       headers: :any,
-    #       methods: [:get, :patch, :put, :delete, :post, :options]
-    #       )
-    #   end
-    # end
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+          )
+      end
+    end
   end
 end
