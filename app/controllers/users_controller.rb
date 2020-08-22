624c71bb51938d354053b27590d41bc72a4122f6
Srishti Nath <srishti.nath@gmail.com>
2020-08-22 13:27:53 -0400

    userRoom relations removed on wronganswers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
     
     
     def wrongGuess
-        @user = User.find(params[:id])
+        @user = User.find(params[:user_id])
         UserRoom.where(user_id: @user.id).destroy_all
         render json: {message: "All user rooms have been removed"}
     end
